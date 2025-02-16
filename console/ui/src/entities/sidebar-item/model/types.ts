22bd3eb67c46fa5a35ed30ab71b5df87115ddc45
Nha Le Truc <nha.letruc@gmail.com>
2025-02-16 11:54:55 +0700

    Remove frontend code

--- console/ui/src/entities/sidebar-item/model/types.ts
+++
@@ -1,8 +0,0 @@
-export interface SidebarItemProps {
-  path: string;
-  label: string;
-  icon?: Element;
-  isActive?: string;
-  isCollapsed?: boolean;
-  target?: string;
-}
--- console/ui/src/shared/model/constants.ts
+++
@@ -1,5 +0,0 @@
-export const AUTHENTICATION_METHODS = Object.freeze({
-  // changing names might break secrets POST request
-  SSH: 'ssh_key',
-  PASSWORD: 'password',
-});
